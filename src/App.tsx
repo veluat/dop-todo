74758e4fa0364db78f742b2220af07ca415b536d
Julia <veluat@gmail.com>
2022-09-30 19:26:47 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type PropsType =
+    {
+        userId: number,
+        id: number,
+        title: string,
+        completed: boolean
+    }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState<Array<PropsType>>([])
+
+
+    useEffect(() =>{ fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(json => setTodos(json))}, [])
+
+
+    const onClickHandler = () => {
+        setTodos([])
+    }
+
+    return (
+        <div className="App">
+            <button onClick={onClickHandler}>NEW POSTS</button>
+            <ul>
+                {todos.map(el => {
+                    return (
+                        <li><span>{el.userId}</span>
+                            <span>{el.id}</span>
+                            <span>{el.title}</span>
+                            <span>{el.completed}</span>
+                        </li>
+                    )
+                })}
+
+            </ul>
+
+
+        </div>
+    );
 }
 
 export default App;
